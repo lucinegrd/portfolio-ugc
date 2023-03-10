5f5d3bd5a9c4c231eecc95daf8a35f011700805b
lucinegrd <giraud.lucine@gmail.com>
2023-03-10 20:28:29 +0100

    photos et vidéos

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,12 +1,39 @@
 import '../styles/Work.css'
 import ReactPlayer from 'react-player'
 import { Text } from '../langues/Text'
+import betterhair from '../asset/photos/betterhair.jpeg'
+import larosee from '../asset/photos/larosee.jpeg'
+import masque from '../asset/photos/masque.jpeg'
+import pen from '../asset/videos/pen.mov'
+import stick from '../asset/videos/stick.mov'
 
 
 function Work() {
     return <div id="work" className='Work'>
         <p className="Work_title"><Text tid="videos" i={-1} /></p>
+        <div className='Videos'>
+            <div className="Videos_item">
+                <ReactPlayer url={pen} controls="true" playing="true" muted="false" width="auto" loop="true" />
+            </div>
+            <div className="Videos_item">
+                <ReactPlayer url={stick} controls="true" playing="true" muted="false" width="auto" loop="true" />
+            </div>
+        </div>
         <p className="Work_title"><Text tid="photos" i={-1} /></p>
+
+        <div className='Photos'>
+            <div className="Photos_item">
+                <img src={betterhair} alt="flacon d'huile pour les cheveux" className='Photos_item_pic' />
+            </div>
+            <div className="Photos_item">
+                <img src={larosee} alt="masque pour le visage, avec un coeur sur ma joue" className='Photos_item_pic' />
+            </div>
+            <div className="Photos_item">
+                <img src={masque} alt="stick solaire" className='Photos_item_pic'/>
+            </div>
+        </div>
+
+
 
     </div>
 }
--- src/langues/Text.jsx
+++ src/langues/Text.jsx
@@ -3,5 +3,5 @@ import { LanguageContext } from './langues';
 
 export function Text({ tid, i }) {
     const languageContext = useContext(LanguageContext);
-    return ((i != -1)&&languageContext.dictionary[tid][i]) || languageContext.dictionary[tid] || tid;
+    return ((i !== -1)&&languageContext.dictionary[tid][i]) || languageContext.dictionary[tid] || tid;
 };
